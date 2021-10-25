d9ca270d45189e805536ef330e5e500cda8147a3
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-10-25 09:53:33 -0600

    initial commit

---
+++ js/projects.js
@@ -0,0 +1,69 @@
+const tech = [
+  {
+    name: "First light",
+    img: "img/first_light/basic.png",
+    live: "https://shawnkelleyart.github.io/FirstLight/",
+    github: "https://github.com/shawnkelleyart/FirstLight",
+    description:
+      "<p>This app is intended to give Christians their first light of the day.</p><p>It's simple, easy to edit, and saves locally in the browser.</p><p>First Light. Find your way for the day.</p>",
+    otherImg: "img/first_light/notes.png",
+  },
+  {
+    name: "Tomorrow Land",
+    img: "",
+    live: "https://shawnkelleyart.github.io/TomorrowLand/",
+    github: "https://github.com/shawnkelleyart/TomorrowLand",
+    description:
+      "<p>Tomorrow Land is a simple, easy to use budgeting app.  It works great on mobile because it keeps input and balance fields where you can see them -- no scrolling required.</p><p>Tomorrow Land: Shape Tomorrow Today.</p>",
+    otherImg: "",
+  },
+  {
+    name: "Random Quotes",
+    img: "img/project-1/green_king.png",
+    live: "https://shawnkelleyart.github.io/techdegree-project-1/",
+    github: "https://github.com/shawnkelleyart/techdegree-project-1",
+    description:
+      "<p>Random Quotes was a coding challenge from Team Treehouse.  The page generates a new quote and randomly-generated color using asynchronous JavaScript.</p>",
+    otherImg: [
+      "img/project-1/purple_einstein.png",
+      "img/project-1/purple_lincoln.png",
+      "img/project-1/red_king.png",
+    ],
+  },
+  {
+    name: "Pagination",
+    img: "img/project-2/search.png",
+    live: "https://shawnkelleyart.github.io/techdegree-project-2/",
+    github: "https://github.com/shawnkelleyart/techdegree-project-2",
+    description:
+      "<p>Student List Pagination was a coding challenge from Team Treehouse.  The page sorts a list of 42 students into pages of 12 and includes search functionality.</p>",
+    otherImg: ["img/project-2/full.png"],
+  },
+];
+
+const music = [
+  {
+    name: "Urban Suite",
+    instrumentation: "EETT",
+    length: "apx. 12 minutes",
+    challenge: "hard",
+    link: "https://www.cimarronmusic.com/urban-suite-8504",
+    img: "",
+  },
+  {
+    name: "An Off Day",
+    instrumentation: "EETT",
+    length: "",
+    challenge: "medium-hard",
+    link: "https://www.cimarronmusic.com/anoffday",
+    img: "",
+  },
+  {
+    name: "Home Suite Home",
+    instrumentation: "EETT",
+    length: "",
+    challenge: "medium-hard",
+    link: "https://www.cimarronmusic.com/homesuitehome",
+    img: "",
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,142 @@
+/*=============
+
+  Declarations
+
+=============*/
+
+const projects = document.getElementById("projects");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const biobox = document.querySelector(".bio-box");
+const blue = "rgb(0, 13, 131)";
+
+/*======
+
+  CB
+
+======*/
+
+function resetColors(ul, bgcolor, textcolor) {
+  const items = ul.children;
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = bgcolor;
+    items[i].style.color = textcolor;
+  }
+}
+
+function toggleBtn(li) {
+  if (li.style.backgroundColor !== "white") {
+    li.style.backgroundColor = "white";
+    li.style.color = blue;
+  } else {
+    li.style.backgroundColor = "";
+    li.style.color = "white";
+  }
+}
+
+function switchTopCalc(element, newpx) {
+  element.style.top = `calc(100vh - ${newpx})`;
+}
+
+function toggleDisplay(element) {
+  if (element.style.display !== "none") {
+    element.style.display = "none";
+  } else {
+    element.style.display = "";
+  }
+}
+
+/*============
+
+  MOBILE NAV
+
+============*/
+
+const navUL = document.querySelector("nav").children[0];
+const li = navUL.children;
+
+//callback for nav listener conditionals
+function writeProjectDisplay(navLI, projectsOb) {
+  resetColors(navUL, blue, "white");
+  toggleBtn(navLI);
+  switchTopCalc(projects, "120px");
+  projects.innerHTML = `<li>Projects</li>`;
+  projects.children[0].style.display = "flex";
+  for (let i = 0; i < projectsOb.length; i++) {
+    projects.insertAdjacentHTML("beforeend", `<li>${projectsOb[i].name}</li>`);
+  }
+  mobileProjectToggle(projects);
+  projectListener(projects);
+}
+
+toggleBtn(li[2]); //default = about
+
+//nav listener cb
+function navigate(e) {
+  if (e.target === li[0]) {
+    writeProjectDisplay(li[0], tech);
+    footer.style.paddingBottom = "120px";
+  } else if (e.target === li[1]) {
+    writeProjectDisplay(li[1], music);
+    footer.style.paddingBottom = "120px";
+  } else if (e.target === li[2]) {
+    resetColors(navUL, blue, "white");
+    toggleBtn(li[2]);
+    footer.style.paddingBottom = "70px";
+    projects.innerHTML = ``;
+    if (biobox.style.display === "none") {
+      toggleDisplay(biobox);
+    }
+  }
+}
+
+//nav listener
+navUL.addEventListener("click", (e) => {
+  navigate(e);
+});
+
+/*============
+
+  PROJECTS
+
+============*/
+
+//  MOBILE PROJECT MENU TOGGLE -- CB in navUL listener
+
+function mobileProjectToggle(ul) {
+  const items = ul.children;
+  const showBtn = items[0];
+  showBtn.addEventListener("click", (e) => {
+    if (items[1].style.display === "none" || items[1].style.display === "") {
+      for (let i = 0; i < items.length; i++) {
+        items[i].style.display = "flex";
+      }
+      let height = items.length * 60;
+      height = height + 60 + "px";
+      switchTopCalc(ul, height);
+      toggleBtn(showBtn);
+    } else {
+      for (let i = 1; i < items.length; i++) {
+        items[i].style.display = "none";
+      }
+      switchTopCalc(ul, "120px");
+      toggleBtn(showBtn);
+    }
+  });
+}
+
+//  PROJECT UL LISTENER / PAGE WRITER -- CB in navUL listener
+
+function projectListener(ul) {
+  ul.addEventListener("click", (e) => {
+    const items = ul.children;
+
+    if (items[0].innerText === "Projects") {
+      if (e.target !== items[0] && biobox.style.display !== "none") {
+        toggleDisplay(biobox);
+      }
+      if (e.target === items[1]) {
+      }
+    }
+  });
+}
